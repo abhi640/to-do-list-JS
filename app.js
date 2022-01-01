310d24aed67f4012d7c5abbd84fad0e102974668
abhi640 <abhi6340@gmail.com>
2022-01-01 11:05:54 +0530

    switch included. current day displaying

--- app.js
+++ app.js
@@ -3,16 +3,49 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  if (currentDay === 6 || currentDay === 0) {
-    res.send ("<h1>Yay</h1>");
-  } else {
-    res.sendFile(__dirname + "/index.html");
-  }
+  var day = "";
+
+switch (currentDay) {
+  case 0:
+  day = "Sunday";
+  break;
+
+  case 1:
+  day = "Monday";
+  break;
+
+  case 2:
+  day = "Tuesday";
+  break;
+
+  case 3:
+  day = "Wednesday";
+  break;
+
+  case 4:
+  day = "Thursday";
+  break;
+
+  case 5:
+  day = "Friday";
+  break;
+
+  case 6:
+  day = "Saturday";
+  break;
+  default:
+  console.log("Error: current day is equal to " + currentDay);
+
+}
+
+  res.render("List", {kindOfDay: day});
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
